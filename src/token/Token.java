8fee75ec079586de46a0b7293e298bdb92e933b2
danie <danie@DESKTOP-HDJ1N3K>
2023-11-23 23:07:15 -0500

    token

---
+++ src/token/Token.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package token;
+
+/**
+ *
+ * @author danie
+ */
+public class Token {
+
+    public static void main(String[] args) {
+       
+        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
+        
+    }
+    
+}
